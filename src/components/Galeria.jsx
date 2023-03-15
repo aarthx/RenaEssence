23588b2384baebee79e694977872870306489eb9
Arthur Augusto <arthuraugusto.exe@gmail.com>
2023-03-15 15:16:02 -0300

    add imgs to dist folder 2

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -5,34 +5,31 @@ const Galeria = () => {
   return (
     <section className={styles.galeria}>
       <div className={styles.conjunto1}>
-        <img src="src/assets/galery/monalisa.png" alt="Monalisa" />
+        <img src="./assets/galery/monalisa.png" alt="Monalisa" />
+        <img src="./assets/galery/virgemDaRocha.png" alt="Virgem das Rochas" />
         <img
-          src="src/assets/galery/virgemDaRocha.png"
-          alt="Virgem das Rochas"
-        />
-        <img
-          src="src/assets/galery/homemVitruviano2.png"
+          src="./assets/galery/homemVitruviano2.png"
           alt="O Homem Vitruviano"
         />
         <img
-          src="src/assets/galery/ultimaCeia.png"
+          src="./assets/galery/ultimaCeia.png"
           alt="A Última Ceia"
           style={{ gridColumn: '-1 / 1' }}
         />
       </div>
       <div className={styles.conjunto2}>
         <img
-          src="src/assets/galery/capelaSistina.png"
+          src="./assets/galery/capelaSistina.png"
           alt="Teto da Capela Sistina"
         />
-        <img src="src/assets/galery/venus.png" alt="O Nascimento de Vênus" />
+        <img src="./assets/galery/venus.png" alt="O Nascimento de Vênus" />
         <img
-          src="src/assets/galery/adao.png"
+          src="./assets/galery/adao.png"
           alt="A criação de Adão"
           style={{ gridColumn: '-1 / 1' }}
         />
-        <img src="src/assets/galery/madona.png" alt="Madona das escadas" />
-        <img src="src/assets/galery/centauromaquia.png" alt="Centauromaquia" />
+        <img src="./assets/galery/madona.png" alt="Madona das escadas" />
+        <img src="./assets/galery/centauromaquia.png" alt="Centauromaquia" />
       </div>
     </section>
   );
